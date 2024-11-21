4573c8c9194f56e7105f1747e55251247e19e271
XiaoMouz <gxiaomouz@gmail.com>
2024-11-21 18:26:59 +0800

    chore: add icp

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -3,7 +3,10 @@
     <div class="container mx-auto py-4">
       <p class="text-center text-gray-500 text-sm">
         © 2024 <a href="https://mou.best" target="_blank">mou.best</a> All
-        Rights Reserved.
+        Rights Reserved. |
+        <a href="https://beian.miit.gov.cn" target="_blank"
+          >闽ICP备2021002999号</a
+        >
       </p>
     </div>
   </footer>
