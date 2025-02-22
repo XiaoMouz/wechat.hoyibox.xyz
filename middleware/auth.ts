64c4dd27d0dfd8d1f30ab67ddb5f5a293d21152d
XiaoMouz <gxiaomouz@gmail.com>
2025-02-22 14:50:15 +0800

    fix: page middleware auth by server

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,14 @@
 import { useAuthStore } from '~/store/auth'
 
-export default defineNuxtRouteMiddleware((to) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const token = useCookie('auth.token')
+
+  // fetch seesion check from server
+
+  const res = await fetch('/api/auth/session')
+  if (res.status !== 200) {
+    return navigateTo('/auth/login')
+  }
 
   if (token.value && to?.name === 'login') {
     return navigateTo('/manage')
--- server/api/auth/session.get.ts
+++ server/api/auth/session.get.ts
@@ -2,5 +2,14 @@ import { ensureAuth } from '~/server/utils/sercret'
 
 export default eventHandler(async (event) => {
   const user = await ensureAuth(event)
-  return user
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Unauthorized',
+    })
+  }
+  setResponseStatus(event, 200)
+  return {
+    status: 'ok',
+  }
 })
